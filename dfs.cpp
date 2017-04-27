82fb8bd9de2315764f0651b04bea6bf6f87ca3d5
Ravikiran <kienme@gmail.com>
2017-04-27 18:15:47 +0530

    Update dfs.cpp Add comments and spacing

--- dfs.cpp
+++ dfs.cpp
@@ -2,40 +2,32 @@
 
 using namespace std;
 
-int n=5, visited[5]={0,0,0,0,0}, count=1;;
+int visited[4] = {0, 0, 0, 0};
 
-/*int G[5][5]={	{0, 1, 1, 1, 0},
-				{1, 0, 0, 1, 0},
-				{1, 0, 0, 0, 1},
-				{1, 1, 0, 0, 1},
-				{0, 0, 1, 1, 0}		};*/
-
-int G[4][4]={	{0, 0, 0, 0},
-				{0, 0, 1, 1},
+int G[4][4]={	{0, 1, 0, 0},
+				{1, 0, 1, 1},
 				{0, 1, 0, 1},
 				{0, 1, 1, 0}	};				
 
 void dfs(int v)
 {
+	//Mark current node as visited and display
 	cout<<v<<" ";
-	visited[v]=1;
+	visited[v] = 1;
 
-	for(int i=0; i<n; ++i)
-		if( (G[v][i]==1) && (visited[i]!=1) )
+	//For each node
+	for(int i = 0; i < 4; ++i)
+		//Check if it is connected to current node and unvisited
+		if( (G[v][i] == 1) && (visited[i] != 1) )
 		{
-			++count;
-			visited[i]=1;
+			//Mark as visited and call DFS
+			visited[i] = 1;
 			dfs(i);
 		}
 }
 			
 int main()
 {
-
-	dfs(1);
-	if(count<n)
-		cout<<"dis";
-	cout<<"connected";
-
+	dfs(2);			//Starting node to perform BFS
 	return 0;
 }
