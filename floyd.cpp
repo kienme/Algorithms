527b42bcb9ff29355ffae482cadf66ad3aa1e5cd
Ravikiran <kienme@gmail.com>
2017-04-28 21:32:56 +0530

    Add comments to floyd.cpp

--- floyd.cpp
+++ floyd.cpp
@@ -1,40 +1,35 @@
 #include<iostream>
-#include<omp.h>
 
 using namespace std;
 
 int main()
 {
-	int G[4][4] = {{0, 999, 3, 999},
-					{2, 0, 999, 999},
-					{999, 7, 0, 1},
-					{6, 999, 999, 0}} ;
+	int G[4][4] = { {0  , 999, 3  , 999},
+					{2  , 0  , 999, 999},
+					{999, 7  , 0  , 1  },
+					{6  , 999, 999, 0  } };
 
-	int shorty[4][4];
+	int dist[4][4];
 
-	omp_set_num_threads(2);
-
+	//Initialize dist with G
 	for(int j=0; j<4; ++j)
 			for(int k=0; k<4; ++k)
-				shorty[j][k] = G[j][k];
-
-	#pragma omp parallel for
+				dist[j][k] = G[j][k];
 
 	for(int i=0; i<4; ++i)
 		for(int j=0; j<4; ++j)
 			for(int k=0; k<4; ++k)
-				if(shorty[j][k]>(shorty[j][i]+shorty[i][k]))
-					shorty[j][k] = shorty[j][i]+shorty[i][k];
+				if(dist[j][k]>(dist[j][i]+dist[i][k]))
+					dist[j][k] = dist[j][i]+dist[i][k];
 
-
+	//Display 
 	for(int j=0; j<4; ++j) 
 	{
 			for(int k=0; k<4; ++k)
-				cout<<shorty[j][k]<<"  ";
+				cout<<dist[j][k]<<"  ";
 
 			cout<<"\n";
 	}
-
 
 	return 0;
 }
