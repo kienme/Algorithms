135df8ca74938e28902d3253cec7897200ab213f
Ravikiran <kienme@gmail.com>
2017-04-29 19:51:03 +0530

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -4,65 +4,54 @@ using namespace std;
 
 int main()
 {
-	
-	/*int G[4][4] = {{0, 8, 2, 4},
-					{8, 0, 5, 999},
-					{2, 5, 0, 3},
-					{4, 999, 3, 0}} ;*/
+	int G[6][6] = { {0  , 3  , 999, 999, 6  , 5  },
+					{3  , 0  , 1  , 999, 999, 4  },
+					{999, 1  , 0  , 6  , 999, 4  },
+					{999, 999, 6  , 0  , 8  , 5  },
+					{6  , 999, 999, 8  , 0  , 2  },
+					{5  , 4  , 4  , 5  , 2  , 0  } };				
 
-	int G[6][6] = {{0, 3, 999, 999, 6, 5},
-					{3, 0, 1, 999, 999, 4},
-					{999, 1, 0, 6, 999, 4},
-					{999, 999, 6, 0, 8, 5},
-					{6, 999, 999, 8, 0, 2},
-					{5, 4, 4, 5, 2, 0}} ;					
+	int parent[6] = { -1, -1, -1, -1, -1, -1 };
 
-	int parent[6] = {0, 0, 0, 0, 0, 0};
 	int num_edges = 1;
 	int min, u, v;
 	int cost = 0;
 
 	while(num_edges < 6)
 	{
-
 		min = 999;
 
-		for(int i=0; i<6; ++i)
-		{
-			for(int k=0; k<6; ++k)
-			{
-				if(G[i][k]<min)
+		for(int i = 0; i < 6; ++i)
+			for(int k = 0; k < 6; ++k)
+				if(G[i][k] < min)
 				{
 					min = G[i][k];
 					u = i;
 					v = k;
 				}
-			}
-		}
-
 		
 		int t_u = u;
 		int t_v = v;
 
-		while(parent[t_u]!=0)
+		while(parent[t_u] != -1)
 			t_u = parent[t_u];
 
-		while(parent[t_v]!=0)
+		while(parent[t_v] != -1)
 			t_v = parent[t_v];
 
-		if(t_u!=t_v)
+		if(t_u != t_v)
 		{
 			num_edges++;
-			cost+=min;
-			parent[v]=u;
+			cost += min;
+			parent[v] = u;
 
-			cout<<v+1<<"->"<<u+1<<"\n";
+			cout<<v<<"->"<<u<<"\n";
 		}
 
 		G[u][v] = G[v][u] = 999;
 	}
 
-	cout<<cost;
+	cout<<"Total cost: "<<cost;
 
 	return 0;
 }
