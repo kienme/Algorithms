0d33b7e8aaec57fa48acaff6fa1fd4eb4a304745
Ravikiran <kienme@gmail.com>
2017-05-01 19:21:30 +0530

    Update subset.cpp
    We've been doing it wrong all this time. DP is just not suited for this problem because we are dependent on the previously computed values. It is possible to have a case where the first element is not included and yet it leads to a solution.

--- subset.cpp
+++ subset.cpp
@@ -2,36 +2,39 @@
 
 using namespace std;
 
-int M = 5;
-/*int val[5]={1, 2, 5, 6, 8};*/
-int val[5]={1, 2, 3, 4, 5};
-int selected[5]={0, 0, 0, 0, 0};
+int M = 7;								//Value of the total sum
+int val[6] = {1, 2, 4, 5, 6, 999};		//Given values in increasing order and last one set to a high value
+int selected[6] = {0, 0, 0, 0, 0, 0};
 
-void sumofsub(int cur_sum, int k, int rem_sum)
+void display()
+{
+	for(int i=0; i<6; ++i)
+		if(selected[i]==1)
+			cout<<val[i]<<" ";
+	cout<<"\n";
+}
+
+void sumofsub(int cur_sum, int k)
 {
 	selected[k]=1;
 
+	//Check if we got M already
 	if(cur_sum+val[k]==M)
-	{
-		for(int i=0; i<5; ++i)
-			if(selected[i]==1)
-				cout<<val[i]<<" ";
-		cout<<"\n";
-	}
+		display();
 
+	//If not, try including next element too
 	else if(cur_sum+val[k]+val[k+1] <= M)
-		sumofsub(cur_sum+val[k], k+1, rem_sum-val[k]);
+		sumofsub(cur_sum+val[k], k+1);
 
-	if( (cur_sum+val[k+1] <= M) && (cur_sum+rem_sum-val[k] >= M) )
-	{
-		selected[k]=0;
-		sumofsub(cur_sum, k+1, rem_sum-val[k]);
-	}
+	//Also check by leaving out current element
+	selected[k] = 0;
+	if(cur_sum+val[k+1] <= M) 
+		sumofsub(cur_sum, k+1);
 }
 
 int main()
 {
-	sumofsub(0, 0, 15);
+	sumofsub(0, 0);
 
 	return 0;
 }
